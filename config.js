3e4a8d46a9494010e44ce20ffefc837e5458c257
Hossein Fadaifard <fadaifard@gmail.com>
2019-05-06 22:07:53 -0500

    Update `config.js`

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 
 exports.config =
 {
-    'username': "admin",
-    'password': "FcRGc4hN670XhSqd",
+    'username': "viewer",
+    'password': "p4ssw0rd",
     'ws_authkey': '0YE07uoK27jNBq92tHADQmw/CS0ayGkk5Ocnjq4+gto=',
     'port': 8080
 };
